c22aa925418af5cb052c8666f1e80d5c89da8f30
Богдан Сава <bogdansava@MacBook-Pro-Bogdan.local>
2021-06-23 21:23:52 +0300

    add function for search all users with prefix test_

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,11 +1,11 @@
 const AWS = require('aws-sdk')
 const cognito = new AWS.CognitoIdentityServiceProvider({region: 'us-east-2'})
 
-const deleteUser = async(sub) => {
+const deleteUser = async(userPollId, username) => {
     return await new Promise((resolve, reject) => {
         const params = {
-            UserPoolId: 'us-east-2_BSmTaxcbR',
-            Username: sub
+            UserPoolId: userPollId,
+            Username: username
         }
 
         cognito.adminDeleteUser(params, (err, data) => {
@@ -18,15 +18,40 @@ const deleteUser = async(sub) => {
     })
 }
 
+const listUsers = async(userPollId, usernamePrefix) => {
+    return await new Promise((resolve, reject) => {
+
+        const usernameFilter = "username ^= " + "\"" + usernamePrefix + "\""
+
+        const params = {
+            UserPoolId: userPollId, 
+            AttributesToGet: null,
+            Filter: usernameFilter,      /* "username ^= \"test_\"" -> ім'я юзеру починається з test_ */
+            Limit: null,
+            PaginationToken: null
+        }
+
+        cognito.listUsers(params, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
 const main = async(event) => {
     console.log('Event:', event)
-    return deleteUser(event.sub)
+    var users = listUsers(event.userPollId, event.usernamePrefix)
+    return users
 }
 
 exports.lambdaHandler = main
 
 // for local use only
-const config = require('./config.json')
-main({
-    sub: config.sub,
-})
+// const config = require('./config.json')
+// main({
+//     userPollId: config.userPollId,
+//     usernamePrefix: config.usernamePrefix
+// })
