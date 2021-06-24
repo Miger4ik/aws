c75bbcbb222f0729752bb0071517692886487c9d
Богдан Сава <bogdansava@MacBook-Pro-Bogdan.local>
2021-06-24 10:56:33 +0300

    fix filter

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,8 +1,9 @@
-const AWS = require('aws-sdk')
-const cognito = new AWS.CognitoIdentityServiceProvider()
+"use strict";
+const AWS = require('aws-sdk');
+const cognito = new AWS.CognitoIdentityServiceProvider();
 
 function cognitoSetRegion(region) {
-    cognito.region = region
+    cognito.region = region;
 }
 
 const deleteUser = async(userPollId, username) => {
@@ -11,22 +12,22 @@ const deleteUser = async(userPollId, username) => {
         const params = {
             UserPoolId: userPollId,
             Username: username
-        }
+        };
 
         cognito.adminDeleteUser(params, (err, data) => {
             if(err) {
-                reject(err)
+                reject(err);
             } else {
-                resolve(data)
+                resolve(data);
             }
-        })
-    })
-}
+        });
+    });
+};
 
 const listUsers = async(userPollId, usernamePrefix) => {
     return await new Promise((resolve, reject) => {
 
-        const usernameFilter = "username ^= " + "\"" + usernamePrefix + "\""
+        const usernameFilter = `username ^= \"${usernamePrefix}\"`; // "username ^= " + "\"" + usernamePrefix + "\"";
 
         const params = {
             UserPoolId: userPollId, 
@@ -34,24 +35,25 @@ const listUsers = async(userPollId, usernamePrefix) => {
             Filter: usernameFilter,      /* "username ^= \"test_\"" -> ім'я юзеру починається з test_ */
             Limit: null,
             PaginationToken: null
-        }
+        };
 
         cognito.listUsers(params, (err, data) => {
             if (err) {
-                reject(err)
+                reject(err);
             } else {
-                resolve(data)
+                resolve(data);
             }
-        })
-    })
-}
+        });
+    });
+};
 
 const main = async(event) => {
-    console.log('Event:', event)
-    cognitoSetRegion(event.region)
-    var users = listUsers(event.userPollId, event.usernamePrefix)
-    return users
-}
+    console.log('Event:', event);
+    cognitoSetRegion(event.region);
+    const users = listUsers(event.userPollId, event.usernamePrefix);
+    // TODO видалення знайдених користувачів
+    return users;
+};
 
-exports.lambdaHandler = main
+exports.lambdaHandler = main;
 // js-lambda-from-vs
